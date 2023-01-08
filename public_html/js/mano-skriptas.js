51aa05c7e30cc201bd5820d2caead106f8de5523
AKV85 <a.kotov.laknojus@gmail.com>
2023-01-09 00:25:35 +0200

    Darbas su html,css ir js. Padaryti 2 puslapiai portfolio ir kontaktai.

---
+++ public_html/js/mano-skriptas.js
@@ -0,0 +1,18 @@
+let a = 1;
+let b= 'Kintamojo a reiksme:';
+let z = b + a;
+console.log(z);
+let c = 2;
+let y = b + c;
+console.log(y);
+let v = 'Andrej Kotov';
+let t = 'Mano Vardas-';
+let vt = t + v;
+console.log(vt);
+let metai = 1985;
+let menuo = 10;
+let diena = 15;
+let liekanaMetai = metai % 10
+let liekanaMenuo = menuo % 10;
+let liekanaDiena = diena % 10;
+console.log('Skaitmenu suma = ' + (liekanaMetai + liekanaMenuo + liekanaDiena))
